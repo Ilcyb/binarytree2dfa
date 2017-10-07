716e2b4c6cf8b7875e7ccefd7704032a58dd2fad
Huangyunbo1996 <hybmail1996@gmail.com>
2017-10-07 11:38:00 +0800

    完善二叉树合法性的验证

--- binary_tree.py
+++ binary_tree.py
@@ -10,6 +10,15 @@ class NodeValue(Enum):
     def has_value(cls, value):
         return (any(value == item.value for item in cls))
 
+    @classmethod
+    def is_binary_operator(cls, value):
+        values = [item.value for item in cls if item != cls.Iteration]
+        return value in values
+
+    @classmethod
+    def is_unary_operator(cls, value):
+        return not cls.is_binary_operator(value)
+
 class BinaryTree:
 
     def __init__(self, node):
@@ -27,9 +36,27 @@ class BinaryTree:
         self.right_tree = right_tree
         self.right_tree.father = self
         self.right_tree.layer = self.layer + 1
+
+    def has_child(self):
+        return self.left_tree != None or self.right_tree != None
+
+    def num_of_child(self):
+        if self.has_child():
+            if self.left_tree != None and self.right_tree != None:
+                return 2
+            else:
+                return 1
+        return 0
     
     def validity(self):
-        if (self.left_tree != None or self.right_tree != None) and not NodeValue.has_value(self.node):
+        # 验证二叉树的非叶子结点是否为指定的值
+        if self.has_child() and not NodeValue.has_value(self.node):
+            return False
+        # 验证以双目运算符为结点的子树是否有两个孩子结点
+        if (NodeValue.is_binary_operator(self.node) and self.num_of_child() != 2):
+            return False
+        # 验证以单目运算符为结点的子树是否只有一个孩子结点
+        if (NodeValue.is_unary_operator(self.node) and self.num_of_child() != 1):
             return False
         return True
 

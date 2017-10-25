abd3e326e18df34880e0b343512e20a5431926e6
ilcyb <hybmail1996@gmail.com>
2017-10-25 10:04:19 +0800

    实现从正则表达式转换为NFA

--- nfa.py
+++ nfa.py
@@ -2,6 +2,13 @@ from stack import Stack
 
 class NFA:
 
+    @staticmethod
+    def insert_into_dict(key, functions, count):
+        if key in functions:
+            functions[key].append(count)
+        else:
+            functions[key] = list((count,))
+
     def __init__(self, expression, functions, father):
         self._name_ = 'N(' + expression + ')'
         self._expression_ = expression
@@ -12,38 +19,77 @@ class NFA:
 
     def parse(self, count=0):
         self.begin = count
+        # 当表达式是单字符串时的处理
         if len(self._expression_) == 1:
             key = (count, self._expression_)
             count += 1
-            self.functions[key] = count
+            NFA.insert_into_dict(key, self.functions, count)
             self.final = count
             return count
+        # 当表达式是迭代时的处理
         if self._expression_[-1:] == '*':
             key = (count, 'ε')
             count += 1
-            self.functions[key] = count
+            NFA.insert_into_dict(key, self.functions, count)
             self.is_iteration_flag = True
             self._expression_ = self._expression_[:-1] # 去掉*
+        # 当表达式含有选择时的处理
+        if '|' in self._expression_:
+            temp_stack = Stack()
+            for c in range(len(self._expression_)):
+                current_char = self._expression_[c]
+                if current_char == '|' and temp_stack.is_empty():
+                    left_expression = self._expression_[:c]
+                    right_expression = self._expression_[c+1:]
+                    left_n = NFA(left_expression, self.functions, self)
+                    count = left_n.parse(count + 1)
+                    right_n = NFA(right_expression, self.functions, self)
+                    count = right_n.parse(count + 1)
+                    key = (self.begin, 'ε')
+                    NFA.insert_into_dict(key, self.functions, left_n.begin)
+                    key = (self.begin, 'ε')
+                    NFA.insert_into_dict(key, self.functions, right_n.begin)
+                    count += 1
+                    key = (left_n.final, 'ε')
+                    NFA.insert_into_dict(key, self.functions, count)
+                    key = (right_n.final, 'ε')
+                    NFA.insert_into_dict(key, self.functions, count)
+                elif current_char == '(':
+                    temp_stack.push(c)
+                elif current_char == ')':
+                    temp_stack.pop()
         for c in self._expression_:
             if not self._stack_.have('(') and c != '(':
-                key = (count, c)
-                count += 1
-                self.functions[key] = count
+                if c == '|' or c == '*':
+                    pass
+                else:
+                    n1 = NFA(c, self.functions, self)
+                    count = n1.parse(count)
             else:
                 if c == ')' and self._stack_.nums_of_element('(') - self._stack_.nums_of_element(')') == 1:
-                    expr = self._stack_.pop_all()[1:]
+                    expr = self._stack_.pop_all()[1:] # 去掉这层括号
                     n2 = NFA(expr, self.functions, self)
                     count = n2.parse(count)
                 else:
                     self._stack_.push(c)
         if self.is_iteration_flag:
             key = (count, 'ε')
-            self.functions[key] = self.begin + 1
+            NFA.insert_into_dict(key, self.functions, self.begin + 1)
             key = (count, 'ε')
             count += 1
-            self.functions[key] = count
+            NFA.insert_into_dict(key, self.functions, count)
             key = (self.begin, 'ε')
-            self.functions[key] = count
+            NFA.insert_into_dict(key, self.functions, count)
         self.final = count
         return count
+
+    def get_result(self):
+        return self.functions
         
+
+if __name__ == '__main__':
+    result = dict()
+    n = NFA('((a|b)|c)',result,None)
+    n.parse()
+    result = n.get_result()
+    print(result)

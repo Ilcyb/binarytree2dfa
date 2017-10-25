37c7fbf5d91e7344c21f99a88029b3baac488cb1
ilcyb <hybmail1996@gmail.com>
2017-10-25 11:49:27 +0800

    修复实现将正则表达式转换为NFA代码的BUG

--- nfa.py
+++ nfa.py
@@ -17,8 +17,22 @@ class NFA:
         self.father = father
         self.is_iteration_flag = False
 
+    # 判断表达式中是否存在一个不在括号中的 '|'
+    def have_independent_choose(self):
+        temp_stack = Stack()
+        for c in range(len(self._expression_)):
+            current_char = self._expression_[c]
+            if current_char == '|' and temp_stack.is_empty():
+                return True
+            elif current_char == '(':
+                temp_stack.push(c)
+            elif current_char == ')':
+                temp_stack.pop()
+        return False
+
     def parse(self, count=0):
         self.begin = count
+        pass_flag = self.have_independent_choose()
         # 当表达式是单字符串时的处理
         if len(self._expression_) == 1:
             key = (count, self._expression_)
@@ -33,12 +47,14 @@ class NFA:
             NFA.insert_into_dict(key, self.functions, count)
             self.is_iteration_flag = True
             self._expression_ = self._expression_[:-1] # 去掉*
-        # 当表达式含有选择时的处理
-        if '|' in self._expression_:
-            temp_stack = Stack()
-            for c in range(len(self._expression_)):
-                current_char = self._expression_[c]
-                if current_char == '|' and temp_stack.is_empty():
+        
+        for c in range(len(self._expression_)):
+            current_char = self._expression_[c]
+            if not self._stack_.have('(') and current_char != '(':
+                if  current_char == '*':
+                    pass
+                # 当表达式含有选择时的处理
+                elif current_char == '|':
                     left_expression = self._expression_[:c]
                     right_expression = self._expression_[c+1:]
                     left_n = NFA(left_expression, self.functions, self)
@@ -54,24 +70,18 @@ class NFA:
                     NFA.insert_into_dict(key, self.functions, count)
                     key = (right_n.final, 'ε')
                     NFA.insert_into_dict(key, self.functions, count)
-                elif current_char == '(':
-                    temp_stack.push(c)
-                elif current_char == ')':
-                    temp_stack.pop()
-        for c in self._expression_:
-            if not self._stack_.have('(') and c != '(':
-                if c == '|' or c == '*':
-                    pass
                 else:
-                    n1 = NFA(c, self.functions, self)
-                    count = n1.parse(count)
+                    if not pass_flag:
+                        n1 = NFA(current_char, self.functions, self)
+                        count = n1.parse(count)
             else:
-                if c == ')' and self._stack_.nums_of_element('(') - self._stack_.nums_of_element(')') == 1:
+                if current_char == ')' and self._stack_.nums_of_element('(') - self._stack_.nums_of_element(')') == 1:
                     expr = self._stack_.pop_all()[1:] # 去掉这层括号
-                    n2 = NFA(expr, self.functions, self)
-                    count = n2.parse(count)
+                    if not pass_flag:
+                        n2 = NFA(expr, self.functions, self)
+                        count = n2.parse(count)
                 else:
-                    self._stack_.push(c)
+                    self._stack_.push(current_char)
         if self.is_iteration_flag:
             key = (count, 'ε')
             NFA.insert_into_dict(key, self.functions, self.begin + 1)
@@ -89,7 +99,7 @@ class NFA:
 
 if __name__ == '__main__':
     result = dict()
-    n = NFA('((a|b)|c)',result,None)
+    n = NFA('((a|b)|(c*))',result,None)
     n.parse()
     result = n.get_result()
     print(result)
